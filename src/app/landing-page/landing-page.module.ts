8745716e86ba4452f0c75b6df71b014d3763bd1d
Zoodigital7 <zoo.york4130@gmail.com>
2021-09-09 12:18:17 -0500

    Multiple Headers Impl

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -6,13 +6,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page.component';
+import { HeaderOneComponent } from './header-one/header-one.component';
+import { HeaderTwoComponent } from './header-two/header-two.component';
+import { HeaderThreeComponent } from './header-three/header-three.component';
+import { HeaderFourComponent } from './header-four/header-four.component';
 
 
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [
+    LandingPageComponent,
+    HeaderOneComponent,
+    HeaderTwoComponent,
+    HeaderThreeComponent,
+    HeaderFourComponent
+  ],
   imports: [
     CommonModule
+  ], exports: [
+    LandingPageComponent,
+    HeaderOneComponent,
+    HeaderTwoComponent,
+    HeaderThreeComponent,
+    HeaderFourComponent
   ]
 })
 export class LandingPageModule { }
